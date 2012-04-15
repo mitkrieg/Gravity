70dea6186732cb9772228e25c8c4576ea72bbfb2
Mitchell Krieger <miak11@hampshire.edu>
2012-04-15 01:46:23 -0400

    Physics Done

--- physics.py
+++ physics.py
@@ -1,31 +1,121 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 
+import pygame
+from pygame.locals import *
 import math
-from math import sin,cos,tan
+import physicsobjects
+from physicsobjects import *
 
-def vec_comp(vec):
-    mag,angle = vec
-    x = mag*cos(angle)
-    y = mag*sin(angle)
-    return x,y
+#Colors
+BLACK = 0,0,0
+RED = 255,0,0
+BLUE = 0,0,255
+GREEN = 0,255,0
+GREY = 80,80,80
 
-def vec_add(*vects):
-    xlist = []
-    ylist = []
-    for vec in vects:
-        x,y = vec_comp(vec)
-        xlist.append(x)
-        ylist.append(y)
-    for i in xlist:
-        xadd += xlist[i]
-        yadd += ylist[i]
-    return xadd,yadd
+#Initial Conditions
+SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+playerpos = 300,300
 
-def vector(x,y):
-    mag = math.sqrt(x**2+y**2)
-    ang = tan(y/x)
-    return mag,ang
+#Initiate Screen
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("Phyics")
+screen.fill(BLACK)
+bounds = screen.get_rect()
+write = pygame.font.Font(None,60)
+
+#Clock
+FPS = 30
+clock = pygame.time.Clock()
+
+done = False
+moving = False
+
+
+#PlanetGroup
+planetGroup = [Planet(16,(50,50),GREEN),Planet(50,(100,100),BLUE)]
+grabbed = None
+player = Player((1,1),playerpos,RED)
+
+while not done:
+    #Input
+    x,y = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            done = True
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+            done = True
+        elif event.type == MOUSEBUTTONDOWN:
+            for planet in planetGroup:
+                rect = planet.rect
+                if rect.collidepoint(pygame.mouse.get_pos()):
+                    grabbed = planet 
+                if grabbed:
+                    planetGroup.remove(grabbed)
+                    planetGroup.append(grabbed)
+        elif event.type == MOUSEBUTTONUP:
+            grabbed = None
+                        
+        if event.type == MOUSEBUTTONDOWN and x in range(0,70) and y in range(550,600):
+            moving = True
+            print moving
+        elif event.type == MOUSEBUTTONDOWN and x in range (680,800) and y in range (550,600):
+            player.resetPos()
+            moving = False
+                
+
+    #Update
+                
+    player.resetAccel()
+    if moving:
+        for planet in planetGroup:
+            player.addAccel(planet)
+        player.update()
+        player.resetAccel()
+    for planet in planetGroup:
+        if planet.rect.collidepoint(player.xpos,player.ypos):
+            moving = False
+            player.resetAccel()
+    if player.xpos <= 1 or player.xpos >=799:
+        moving = False
+        player.resetAccel()
+    elif player.xpos <= 1 or player.ypos >=599:
+        moving = False
+        player.resetAccel()
         
-def distance((x1,y1),(x2,y2)):
-    dist = math.sqrt((y1-y2)**2+(x1-x2)**2)
-    return dist
+
+    #Draw
+    screen.fill(BLACK)
+
+    pygame.draw.rect(screen,GREY,(0,550,70,50))
+    text = write.render("GO", True, BLACK,GREY)
+    location = text.get_rect()
+    location.bottomleft = bounds.bottomleft
+    screen.blit(text, location)
+
+    pygame.draw.rect(screen,GREY,(680,550,120,50))
+    text = write.render("RESET", True, BLACK,GREY)
+    location = text.get_rect()
+    location.bottomright = bounds.bottomright
+    screen.blit(text, location)    
+
+    
+
+    for planet in planetGroup:
+        others = planetGroup[:]
+        others.remove(planet)
+        if planet == grabbed:
+            planet.rect.center = pygame.mouse.get_pos()
+            planet.rect.clamp_ip(bounds) #ip means in place, keeps rectangle w/in bounds
+        planet.update(screen,planet.rect)
+
+
+    player.draw(screen)
+
+
+    #Refresh
+    pygame.display.flip()
+    clock.tick()
+
+print "END"
---
+++ physicsobjects.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *
+from pygame.sprite import Sprite, Group
+
+import math
+
+GRAVITYCO = 9e2
+
+class Player(Sprite):
+
+    def __init__(self,vec,pos,color):
+        self.vx,self.vy = vec
+        self.vxi,self.vyi = vec
+        self.xpos,self.ypos = pos
+        self.xi,self.yi = pos
+        self.color = color
+        self.ax = 0
+        self.ay = 0
+    
+    def resetAccel(self):
+        self.ax=0
+        self.ay=0
+    
+    def resetPos(self):
+        self.xpos = self.xi
+        self.ypos = self.yi
+        self.vx = self.vxi
+        self.vy = self.vyi
+        self.resetAccel()
+
+    def addAccel(self,planet):
+        planx,plany = planet.rect.center 
+        dx = planx - self.xpos
+        dy = plany - self.ypos
+        dsq = dx*dx + dy*dy
+        dist = math.sqrt(dsq)
+        force = GRAVITYCO*planet.mass/dsq if dsq>1e-10 else 0
+        self.ax += force*dx/dist
+        self.ay += force*dy/dist
+        print self.ax,self.ay
+
+    def update(self):
+        self.xpos += self.vx
+        self.ypos += self.vy
+        self.vx += self.ax
+        self.vy += self.ay
+        self.resetAccel()
+
+    def draw(self,screen):
+        pygame.draw.circle(screen,self.color,(int(self.xpos),int(self.ypos)),2)
+
+    def dead(self):
+        pass
+
+class Planet(Sprite):
+    
+    def __init__(self,mass,pos,color):
+        self.mass = mass
+        self.xpos,self.ypos = pos
+        self.rect = Rect(self.xpos+(self.mass/2),self.ypos+(self.mass/2),self.mass,self.mass)
+        self.color = color
+    
+    def update(self,screen,rect):
+        self.rect = rect
+        pygame.draw.rect(screen,self.color,rect)
+
