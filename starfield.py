50068a1064a8b4662877da1efef12edfb0e65e01
Evan Ricketts <ejr10@hampshire.edu>
2012-04-28 23:44:53 -0400

    rmd dead code

--- starfield.py
+++ starfield.py
@@ -21,27 +21,3 @@ class Starfield(object):
         for star in self.stars:
             self.screen.fill(star[3],(star[0],star[1],star[2],star[2]))   
 
-'''
-class Planet(Sprite):
-    image = None
-    def __init__(self,x,y,mass,group,image=str('rockplanet.jpg')):
-        Sprite.__init__(self)
-        if self.image == None:
-            self.image = system.load_graphics(image)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y 
-        self.group = group
-        self.add(self.group)
-                #self.screen = place
-        self.mass = mass
-
-    def pullPlayer(self,x,y):
-        dist = distance(self.x,self.y,x,y)
-        self.pull = self.mass/dist**2
-        return self.pull
-        pass
-
-    def movePlanet(self):
-        pass
-'''
